f2b1ecbf0b57249b9f952341866668379873189b
grimtin10 <grimtin10@gmail.com>
2025-09-23 22:29:17 -0600

    init + inputs

---
+++ src/controllers.rs
@@ -0,0 +1,144 @@
+use std::{collections::HashSet, sync::{mpsc::{self, Receiver}, Arc, Mutex}, thread, time::{Duration, Instant}};
+
+use gilrs::{Axis, Button, Event, EventType, GamepadId, Gilrs};
+
+macro_rules! push_event {
+    ($event:expr, $sender:expr) => {
+        if $sender.send($event).is_err() {
+            break;
+        }
+    };
+}
+
+#[derive(Debug, Clone)]
+pub enum ControllerEventType {
+    ControllerConnected,
+    ControllerDisconnected,
+    ButtonPressed(Button),
+    ButtonReleased(Button),
+    AxisMotion { axis: Axis, value: f32 },
+}
+
+#[derive(Debug, Clone)]
+pub struct ControllerEvent {
+    pub controller: GamepadId,
+    pub event: ControllerEventType,
+    pub timestamp: u128,
+}
+
+pub struct ControllerManager {
+    receiver: Receiver<ControllerEvent>,
+    controllers: Arc<Mutex<HashSet<GamepadId>>>,
+    #[allow(dead_code)]
+    input_thread: thread::JoinHandle<()>,
+}
+
+impl ControllerManager {
+    pub fn new() -> Result<Self, gilrs::Error> {
+        let (sender, receiver) = mpsc::channel();
+        let controllers = Arc::new(Mutex::new(HashSet::new()));
+        let shared_controllers = controllers.clone();
+
+        let mut gilrs = Gilrs::new()?;
+        
+        for (_id, gamepad) in gilrs.gamepads() {
+            if gamepad.is_connected() {
+                if let Ok(mut guard) = shared_controllers.lock() {
+                    guard.insert(_id.into());
+                }
+            }
+        }
+
+        let start = Instant::now();
+
+        let input_thread = thread::spawn(move || {
+            println!("Controller input thread started.");
+
+            loop {
+                while let Some(Event { id, event, time, .. }) = gilrs.next_event() {
+                    let timestamp = start.elapsed().as_micros();
+
+                    match event {
+                        EventType::Connected => {
+                            println!("Controller {} connected", id);
+                            
+                            if let Ok(mut guard) = shared_controllers.lock() {
+                                guard.insert(id);
+                            }
+                            push_event!(
+                                ControllerEvent {
+                                    controller: id,
+                                    event: ControllerEventType::ControllerConnected,
+                                    timestamp,
+                                },
+                                sender
+                            );
+                        }
+                        EventType::Disconnected => {
+                            println!("Controller {} disconnected", id);
+                            
+                            if let Ok(mut guard) = shared_controllers.lock() {
+                                guard.remove(&id);
+                            }
+                            push_event!(
+                                ControllerEvent {
+                                    controller: id,
+                                    event: ControllerEventType::ControllerDisconnected,
+                                    timestamp,
+                                },
+                                sender
+                            );
+                        }
+                        EventType::ButtonPressed(button, _code) => {
+                            push_event!(
+                                ControllerEvent {
+                                    controller: id,
+                                    event: ControllerEventType::ButtonPressed(button),
+                                    timestamp,
+                                },
+                                sender
+                            );
+                        }
+                        EventType::ButtonReleased(button, _code) => {
+                            push_event!(
+                                ControllerEvent {
+                                    controller: id,
+                                    event: ControllerEventType::ButtonReleased(button),
+                                    timestamp,
+                                },
+                                sender
+                            );
+                        }
+                        EventType::AxisChanged(axis, value, _code) => {
+                            push_event!(
+                                ControllerEvent {
+                                    controller: id,
+                                    event: ControllerEventType::AxisMotion { axis, value },
+                                    timestamp,
+                                },
+                                sender
+                            );
+                        }
+                        _ => {}
+                    }
+                }
+
+                thread::sleep(Duration::from_micros(100));
+            }
+        });
+
+        Ok(Self {
+            receiver,
+            controllers,
+            input_thread,
+        })
+    }
+
+    pub fn drain_events(&mut self) -> Vec<ControllerEvent> {
+        self.receiver.try_iter().collect()
+    }
+
+    pub fn connected_controllers(&self) -> HashSet<GamepadId> {
+        self.controllers.lock().unwrap().clone()
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,153 @@
+mod controllers;
+
+use std::time::Instant;
+
+use gilrs::Button;
+use macroquad::prelude::*;
+
+use crate::controllers::{ControllerEventType, ControllerManager};
+
+const START_X: [f32; 5] = [576.0, 608.0, 640.0, 672.0, 704.0];
+const END_X: [f32; 5]   = [435.2, 536.6, 640.0, 742.4, 844.8];
+
+struct FretAssets {
+    pub fret: Texture2D,
+    pub fret_pressed: Texture2D,
+    pub pressed: Texture2D,
+    pub ring: Texture2D,
+    pub shell: Texture2D,
+}
+
+struct Assets {
+    pub note: Texture2D,
+
+    pub frets: Vec<FretAssets>,
+}
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Open Hero".into(),
+        window_width: 1280,
+        window_height: 720,
+        window_resizable: false,
+        
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    let start = Instant::now();
+
+    let assets = load_assets("assets").await;
+
+    println!("Loading assets took {}ms", (Instant::now()-start).as_millis());
+
+    let mut controllers = ControllerManager::new().unwrap();
+
+    let mut pressed: [bool; 5] = [false, false, false, false, false];
+
+    loop {
+        for event in controllers.drain_events() {
+            match event.event {
+                ControllerEventType::ButtonPressed(button) => {
+                    match button {
+                        Button::West =>        pressed[0] = true,
+                        Button::South =>       pressed[1] = true,
+                        Button::North =>       pressed[2] = true,
+                        Button::East =>        pressed[3] = true,
+                        Button::LeftTrigger => pressed[4] = true,
+                        _ => {}
+                    }
+                }
+                ControllerEventType::ButtonReleased(button) => {
+                    match button {
+                        Button::West =>        pressed[0] = false,
+                        Button::South =>       pressed[1] = false,
+                        Button::North =>       pressed[2] = false,
+                        Button::East =>        pressed[3] = false,
+                        Button::LeftTrigger => pressed[4] = false,
+                        _ => {}
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        clear_background(BLACK);
+
+        for i in 0..5 {
+            render_fret(&assets, i, pressed[i]);
+        }
+
+        for i in 0..5 {
+            render_note(&assets, i, 0.0);
+            render_note(&assets, i, 0.333);
+            render_note(&assets, i, 0.9);
+            // render_note(&assets, i, (mouse_position().1-305.0)/(655.0-305.0));
+        }
+
+        draw_fps();
+        
+        next_frame().await;
+    }
+}
+
+async fn load_assets(folder: &'static str) -> Assets {
+    let mut frets = Vec::new();
+
+    for i in 0..3 {
+        frets.push(FretAssets {
+            fret: load_texture(&format!("{folder}/{i}_fret.png")).await.unwrap(),
+            fret_pressed: load_texture(&format!("{folder}/{i}_fret_pressed.png")).await.unwrap(),
+            pressed: load_texture(&format!("{folder}/{i}_pressed.png")).await.unwrap(),
+            ring: load_texture(&format!("{folder}/{i}_ring.png")).await.unwrap(),
+            shell: load_texture(&format!("{folder}/{i}_shell.png")).await.unwrap()
+        });
+    }
+
+    Assets {
+        note: load_texture(&format!("{folder}/note.png")).await.unwrap(),
+        frets
+    }
+}
+
+fn render_fret(assets: &Assets, fret: usize, pressed: bool) {
+    let textures = if fret == 0 || fret == 4 {
+        &assets.frets[0]
+    } else if fret == 1 || fret == 3 {
+        &assets.frets[1]
+    } else {
+        &assets.frets[2]
+    };
+
+    let flip = fret > 2;
+
+    let x = END_X[fret];
+    let y = 655.0;
+
+    if pressed {
+        render_texture(&textures.pressed, x, y, 0.8, flip);
+    } else {
+        render_texture(&textures.shell, x, y, 0.8, flip);
+        render_texture(&textures.fret, x, y, 0.8, flip);
+        render_texture(&textures.ring, x, y, 0.8, flip);
+    }
+}
+
+fn lerp(a: f32, b: f32, t: f32) -> f32 { a + t * (b - a) }
+
+fn render_texture(texture: &Texture2D, x: f32, y: f32, scale: f32, flip_x: bool) {
+    let width = texture.width() * scale;
+    let height = texture.height() * scale;
+
+    draw_texture_ex(texture, x - width / 2.0, y - height / 2.0, WHITE, DrawTextureParams {
+        dest_size: Some(vec2(width, height)),
+        flip_x,
+        ..Default::default() 
+    });
+}
+
+fn render_note(assets: &Assets, fret: usize, t: f32) {
+    render_texture(&assets.note, lerp(START_X[fret], END_X[fret], t), lerp(305.0, 655.0, t), lerp(0.4, 1.2, t) * 0.629, false);
+}
